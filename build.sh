e3047d07330c88b7a68191312b79a9876ef2ef63
sanjeev.bhandari <realsanjeev2@gmail.com>
2025-12-07 01:14:42 +0545

    feat: Restructure the folder structure and update build script to use `src` and `include` directories.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -xe
 CFLAGS=" -Wall -Wextra -ggdb  -Wno-missing-braces"
 
-cc $CFLAGS -o main main.c game.c -lX11 -lXrandr -lm -lpulse -lpulse-simple
+cc $CFLAGS -Iinclude -o main src/main.c src/game.c -lX11 -lXrandr -lm -lpulse -lpulse-simple
